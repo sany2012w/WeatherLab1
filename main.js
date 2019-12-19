24963a118db7111cf076aaee760c3518909de07e
sany2012w <35635355+sany2012w@users.noreply.github.com>
2019-12-19 14:18:09 +0300

    fetch + promisses

--- main.js
+++ main.js
@@ -1,43 +1,23 @@
 function buttonClick() {
 
-    let cityName = document.getElementById("input").value;
-
-    let request = new XMLHttpRequest();
-
-    let requestText = "https://api.openweathermap.org/data/2.5/weather" +
-        "?q=" + cityName +
-        "&appid=7825ce4ffa896c5019e53087c858568a" +
-        "&units=metric" +
-        "&lang=en";
-    request.open("GET", requestText);
-    request.responseType = "json";
-
-    request.send();
     request.onload = function () {
         if (request.status == 200) {
+            let Source = document.getElementById("weather").innerHTML;
             let MyJson = GetSimpleJson(request.response);
 
-            display(MyJson);
+            displayJson(Source, MyJson);
         }
         else {
+            let Source = document.getElementById("error-message").innerHTML;
             let MyJson = GetErrorMessage(request.response);
 
-            displayError(MyJson);
+            displayJson(Source, MyJson);
         }
     }
 }
 
-function display(MyJson) {
-    let source = document.getElementById("weather").innerHTML;
-    let template = Handlebars.compile(source);
-
-    let html = template(MyJson);
-    document.getElementById("weather-container").innerHTML = html;
-}
-
-function displayError(MyJson) {
-    let source = document.getElementById("error-message").innerHTML;
-    let template = Handlebars.compile(source);
+function displayJson(Source, MyJson) {
+    let template = Handlebars.compile(Source);
 
     let html = template(MyJson);
     document.getElementById("weather-container").innerHTML = html;
@@ -62,3 +42,36 @@ function GetErrorMessage(response) {
   }
   return message;
 }
+
+function getWeather(cityName) {
+    const requestText = "https://api.openweathermap.org/data/2.5/weather" +
+        "?q=" + cityName +
+        "&appid=7825ce4ffa896c5019e53087c858568a" +
+        "&units=metric" +
+        "&lang=en";
+    return fetch(requestText);
+}
+window.onload = () => {
+    const input = document.getElementById("input-onsubmit");
+    input.addEventListener("submit", onSubmit);
+}
+
+function onSubmit(ev) {
+    ev.preventDefault();
+    const city = ev.currentTarget.elements.input.value;
+    getWeather(city).then(response => {
+            response.json().then(json => {
+                if (response.ok) {
+                    let Source = document.getElementById("weather").innerHTML;
+                    let MyJson = GetSimpleJson(json);
+
+                    displayJson(Source, MyJson);
+                }else {
+                    let Source = document.getElementById("error-message").innerHTML;
+                    let MyJson = GetErrorMessage(json);
+                    
+                    displayJson(Source, MyJson);
+                }
+            })})
+}
+
